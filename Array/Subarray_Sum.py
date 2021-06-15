40b8522ce3aa67acae1524d123ebefb76abea1c2
rav-maker <66152096+rav-maker@users.noreply.github.com>
2021-06-15 22:10:36 +0530

    Update Subarray_Sum.py

--- Array/Subarray_Sum.py
+++ Array/Subarray_Sum.py
@@ -27,14 +27,14 @@ if __name__ == "__main__":
     # test case 1
     arr = [2,3,7,8,5]
     S = 15
-    print(subArraySum(S,arr,len(arr)))
+    print(subArraySum(S,arr,len(arr)))    ## Output = (3,4)
 
     # test case 2
     arr1 = [1,2,3,4,5,6,7,8,9,10]
     S1 = 15
-    print(subArraySum(S1,arr1,len(arr1)))
+    print(subArraySum(S1,arr1,len(arr1)))   ## Output = (1,5)
 
     # test case 3
     arr2  = [1,2,3,7,5]
     S2 = 12
-    print(subArraySum(S2,arr2,len(arr2)))
+    print(subArraySum(S2,arr2,len(arr2)))   ## Output = (2,4)
